e3aa23ab85dd5035c988e0e47f6787d30f5309f1
pepe <jrpenaseco@gmail.com>
2021-02-22 17:40:22 +0000

    Finished static files with  AWSS3

--- config/settings/production.py
+++ config/settings/production.py
@@ -39,7 +39,7 @@ USE_TZ = True
 #STATIC AND MEDIA FILES
 
 
-STATIC_URL = '/api/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"config/static")
@@ -53,12 +53,12 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_BUCKET_NAME')
 AWS_S3_REGION_NAME="eu-west-2"
 AWS_S3_HOST = "s3.eu-west-2.amazonaws.com"
 
-""" STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage' """
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_S3_PATH = 'static/'
 
-
+AWS_LOCATION = 'static'
 
 #CONNECTIONS CONFIGURATION
 
