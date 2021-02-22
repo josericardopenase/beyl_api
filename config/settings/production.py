ba60c42f1121e9473d1fa302e7b05d6e5d3632c9
pepe <jrpenaseco@gmail.com>
2021-02-22 17:47:38 +0000

    Changed region in aws

--- config/settings/production.py
+++ config/settings/production.py
@@ -50,15 +50,11 @@ AWS_ACCESS_KEY_ID        = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY   = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_BUCKET_NAME')
 
-AWS_S3_REGION_NAME="eu-west-2"
-AWS_S3_HOST = "s3.eu-west-2.amazonaws.com"
+AWS_S3_REGION_NAME="eu-west-1"
+AWS_S3_HOST = "s3.eu-west-1.amazonaws.com"
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-STATIC_S3_PATH = 'static/'
-
-AWS_LOCATION = 'static'
 
 #CONNECTIONS CONFIGURATION
 
